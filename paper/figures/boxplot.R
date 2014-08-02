c52e099082a0b1ea3b6291b26280644584a6e263
Douglas Anderson <dougjamesanderson@gmail.com>
2014-08-02 13:14:22 -0400

    Added a license. Stubbed the changed Fei Song recomended. Fixed the data for the boxplot

--- paper/figures/boxplot.R
+++ paper/figures/boxplot.R
@@ -12,13 +12,15 @@ xlab <- "Experiment"
 ylab <- "F-score"
 groups <- c("All", "Just Stopwords", "Just Uncommon", "Just Normalization", "Baseline")
 
-p <- ggplot(data, aes(x=factor(Label), y=F)) + geom_boxplot()
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+palette <- c("#999999", "#84B237", "#6E98FF", "#B24932", "#F0E442")
+
+p <- ggplot(data, aes(x=factor(Label), y=F, fill=factor(Label))) + geom_boxplot()
 p = p + scale_x_discrete(xlab, labels=groups)
 p = p + scale_y_continuous(ylab, limits=c(0.35, 0.55))
 p = p + ggtitle(chart_title)
+p = p + scale_fill_manual(values=palette, guide=FALSE)
 p = p + theme_minimal()
 
-#png("boxplot.png")
 p # Draw that sucker
-#dev.off()
 ggsave("boxplot.png")
